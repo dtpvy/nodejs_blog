1c9b5dfa255f354d4f754c85ef8832b0eb4429d4
dtpvy <dtpy0202@gmail.com>
2022-01-10 20:15:12 +0700

    express handlebars, node sass, scss, bootstrap

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const morgan = require('morgan');
+const path = require('path');
+const app = express()
+const port = 3000
+//--------------------------------------------//
+//HTTP logger
+app.use(morgan('combined'));
+
+// STATIC 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// TEMPLATE ENGINE
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({ extname: '.handlebars' })
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars');
+app.set("views", path.join(__dirname, 'resources\\views')); // cách mình tìm đến file, hệ điều hành window
+
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
