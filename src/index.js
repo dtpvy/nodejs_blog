bfe02169fcb2ddbc17e559d542c6a2a977adb4bd
dtpvy <dtpy0202@gmail.com>
2022-01-11 12:56:07 +0700

    routers and controllers (MVC)

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-const express = require('express')
+const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const app = express()
-const port = 3000
+const route = require('./routers');
+const app = express();
+const port = 3000;
 //--------------------------------------------//
 //HTTP logger
 app.use(morgan('combined'));
 
 // STATIC 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // TEMPLATE ENGINE
-const exphbs = require('express-handlebars')
-const hbs = exphbs.create({ extname: '.handlebars' })
-app.engine('handlebars', hbs.engine)
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ extname: '.handlebars' });
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set("views", path.join(__dirname, 'resources\\views')); // cách mình tìm đến file, hệ điều hành window
 
 
-
-app.get('/', (req, res) => {
-  res.render('home')
-})
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
