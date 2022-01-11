2606a26cec25bf8c3f94a7399da87e33a1841b70
dtpvy <dtpy0202@gmail.com>
2022-01-11 19:01:33 +0700

    Prettier husky lint-staged test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 //HTTP logger
 app.use(morgan('combined'));
 
-// STATIC 
+// STATIC
 app.use(express.static(path.join(__dirname, 'public')));
 
 // TEMPLATE ENGINE
@@ -16,11 +16,10 @@ const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ extname: '.handlebars' });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.set("views", path.join(__dirname, 'resources\\views')); // cách mình tìm đến file, hệ điều hành window
-
+app.set('views', path.join(__dirname, 'resources\\views')); // cách mình tìm đến file, hệ điều hành window
 
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
